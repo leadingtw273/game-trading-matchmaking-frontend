b68801aa2fbecda918fef2a10f6c6950504b41e6
mike <mike-rnd@skvip.org>
2023-10-13 18:33:34 +0800

    feat: 調整 scss 配置

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "@/assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./style.scss";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="app">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,7 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";
+                         @import "@/styles/mixins.scss";`,
+      },
+    },
+  },
+});
