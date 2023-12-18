770f1208c7d0f46daa0295b3747ce5fed0ab5942
mike <mike-rnd@skvip.org>
2023-12-19 00:01:06 +0800

    feat: 完成基本搜索卡片切版

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,4 +1,68 @@
-Component.displayName = "Layout";
+import logo from "@/assets/logo.png";
+import "./style.scss";
+import { Button, Card, Input, Tabs, TabsProps } from "antd";
+
+const items: TabsProps["items"] = [
+  {
+    key: "1",
+    label: "外觀",
+  },
+  {
+    key: "2",
+    label: "角色",
+  },
+  {
+    key: "3",
+    label: "金幣",
+  },
+];
+
+Component.displayName = "Home";
 export function Component() {
-  return <div className="default-content">Home</div>;
+  return (
+    <div className="home">
+      <div className="search-block">
+        <img className="search-block_logo" src={logo} alt="logo-text" />
+        <Card className="search-block_card">
+          <Tabs defaultActiveKey="1" items={items} tabBarStyle={{ color: "red" }} />
+          <Input placeholder="搜尋" prefix={<span className="icon material-symbols-outlined">search</span>} />
+          <Button type="primary">搜尋</Button>
+        </Card>
+      </div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+      <div>橫向卡片列表</div>
+    </div>
+  );
 }
