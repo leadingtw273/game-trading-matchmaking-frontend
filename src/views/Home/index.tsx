52d969192cf59e21f66e4f24e5c0340b26ef903f
Yipin930 <149990734+Yipin930@users.noreply.github.com>
2024-01-14 22:13:20 +0800

    Add files via upload

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,21 +1,29 @@
-// views/home/index.tsx
-
+// views/home/components/search-card/index.tsx
+import { /*Button,*/ Input } from "antd";
+import React from 'react';
 import './style.scss';
-import background from './Background.png';  // 引入背景图片
-import LogoTextFrame from './component/logo-text-frame';
-import SearchCard from './component/search-card'
 
-
-Component.displayName = "Home";
-export function Component() {
+const SearchCard: React.FC = () => {
   return (
-    <div>
-      <div className="background-image" style={{ backgroundImage: `url(${background})` }}>
-      <>
-      <LogoTextFrame />
-      <SearchCard/>
-      </>
-      </div>
+    
+<div className="search-card-image">
+  <h1>
+    <div className="text-frame">
+      <div className="text-1">外觀</div>
+      <div className="text-2">角色</div>
+      <div className="text-3">金幣</div>
     </div>
+  </h1>
+    <div className="search-text-box">
+
+      <Input placeholder="搜尋" prefix={<span className="icon material-symbols-outlined">search</span>} />
+
+    </div>
+</div>
   );
 };
+
+export default SearchCard;
+
+
+
